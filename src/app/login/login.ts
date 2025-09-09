d80795968b797fee6e705d5b9876f091df57de84
nahush p <nahushpatankar17@gmail.com>
2025-09-09 12:25:41 +0530

    my second commit

---
+++ src/app/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+}
