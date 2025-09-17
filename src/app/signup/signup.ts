9411bda8a7739e81a9a56eb129806fe05d454239
nahush p <nahushpatankar17@gmail.com>
2025-09-17 15:51:11 +0530

    my commit

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -41,7 +41,7 @@ export class SignupComponent {
       this.isLoading = true;
       const signupData = { ...this.signupForm.value, role: 'CUSTOMER' };
 
-      this.http.post('http://localhost:8763/auth/register', signupData).subscribe({
+      this.http.post('http://localhost:8763/auth/register', signupData, { responseType: 'text'}).subscribe({
         next: (response: any) => {
           this.showNotificationMessage('Signup successful! Redirecting to login...', 'success');
           setTimeout(() => {
