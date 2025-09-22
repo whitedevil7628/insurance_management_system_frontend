855c428a3132920c147fbf9d9e294062e61c2ce2
Shiv G <shivgupta.cse14@gmail.com>
2025-09-22 10:04:31 +0530

    Interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    // HTTP Client with Auth Interceptor - Automatically adds JWT to all requests
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
